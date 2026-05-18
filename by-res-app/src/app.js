8a42aaf6395a853cec9f05080b346e926e04d40c
Tolga BAYRAK <bayraktolga28@gmail.com>
2026-05-18 23:17:16 +0300

    feat: morgan added

--- by-res-app/src/app.js
+++ by-res-app/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import userRouter from './modules/user/user.router.js';
 import noteRouter from './modules/note/note.router.js';
 import { errorHandler } from './middlewares/error.middleware.js';
@@ -7,6 +8,7 @@ import { errorHandler } from './middlewares/error.middleware.js';
 const app = express();
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.get('/health', (_, res) => res.json({ status: 'ok' }));
